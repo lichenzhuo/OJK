6a1c6faf8fda2ea176f3c7430ac6d2a775dd4526
申学伟 <shenxuewei@adpanshi.com>
2019-03-26 12:30:47 +0800

    online

--- src/config.js
+++ src/config.js
@@ -75,7 +75,7 @@ if (fullCurrentDomain.indexOf(ynDEV) > -1) {
   first_unit = 'Rp';
   last_unit = '';
 } else if (fullCurrentDomain.indexOf(vnPRO) > -1) {
-  domain = `http://api.dongabc.vn`;
+  domain = `http://api.dongabc.com`;
   headerTotal = 'My Dong';
   lang = 'vi';
   first_unit = '';
